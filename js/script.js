7cacf13fcf4fc09db2a1f33103112dab76e5f8e2
Katherine Love <270010932@bun2-206-14.local>
2019-06-28 11:35:58 +1200

    Richards help

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+  var started = false;
+
+  $('#start').click(function(){
+    if(started === false){
+      $(this).fadeOut(1000);
+      $('.getStart').addClass('started');
+      $('.getStartContent').addClass('startedContent');
+      $('#contentContainer').removeClass('contentContainerStart');
+      setTimeout(function(){
+        started = true;
+      }, 1000)
+    }
+  })
+
+  $('#logo').click(function(){
+    if(started === true){
+      $('#start').fadeIn(1000);
+      $('.getStart').removeClass('started');
+      $('.getStartContent').removeClass('startedContent');
+      $('#contentContainer').addClass('contentContainerStart');
+      setTimeout(function(){
+        started = false;
+      }, 1000)
+    }
+
+  })
+
+
+
+
+});
