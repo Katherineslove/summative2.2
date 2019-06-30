0b76fc6ee1fd820dce11bcf30f9c986f64f27e24
Katherine Love <270010932@bun2-206-14.local>
2019-07-01 11:42:28 +1200

    Updates

--- js/script.js
+++ js/script.js
@@ -56,24 +56,23 @@ $(document).ready(function(){
 
    //-----------------------------------------
 
-   function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
 
-     function filterFunction() {
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  div = document.getElementById("myDropdown");
-  a = div.getElementsByTagName("a");
-  for (i = 0; i < a.length; i++) {
-    txtValue = a[i].textContent || a[i].innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      a[i].style.display = "";
-    } else {
-      a[i].style.display = "none";
-    }
-  }
-}
+    var input = document.getElementById('autoComplete');
+    var autoComplete = new google.maps.places.Autocomplete(input);
+    autoComplete.addListener('place_changed', function(){
+      console.log('the place has been changed');
+      var place = autoComplete.getPlace();
+      console.log(place);
+    });
+
+    $("#card").click(function(){
+
+    $(".card-body").toggle();
+
+    });
 
 });
+
+
+
+google.maps.event.addDomListener(window, 'load');
